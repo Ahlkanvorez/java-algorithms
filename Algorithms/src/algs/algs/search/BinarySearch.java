c155dbfd8d89d0cc05913393e10b37e3d7e96b3a
Robert Mitchell <robert.mitchell36@gmail.com>
2016-11-02 22:11:52 -0500

    Implemented Binary Search in a new package for search algorithms.

---
+++ Algorithms/src/algs/algs/search/BinarySearch.java
@@ -0,0 +1,44 @@
+package algs.algs.search;
+
+/**
+ * Created by Robert Mitchell on 11/2/16.
+ */
+public class BinarySearch {
+
+    /**
+     * Implements the Binary Search algorithm for finding an element of a list of objects which implement Comparable.
+     * This search assumes the data given is sorted, and will not function properly on unsorted data. Moreover, it
+     * assumes that target is not null, and will throw a NullPointerException if target is null.
+     *
+     * This method runs using O(lg N) comparisons, and no extra space; it is not recursive so it does not use stack
+     * space.
+     *
+     * @param target The element to search for in the array.
+     * @param data The array in which to search. This must be a sorted array.
+     * @param <T> The type of element to search for, which must implement Comparable
+     * @return The index of target, or the negative of 1 + the index in which it would appear.
+     */
+    public static <T extends Comparable<T>> int find(final T target, final T[] data) {
+        int low = 0;
+        int high = data.length;
+        while (low < high) {
+            final int mid = low + (high - low) / 2;
+            if (target.compareTo(data[mid]) < 0) {
+                high = mid;
+            } else if (target.compareTo(data[mid]) > 0) {
+                low = mid + 1;
+            } else {
+                return mid;
+            }
+        }
+        return -(low + 1);
+    }
+
+    public static void main(final String[] args) {
+        final Integer[] data = { 1, 2, 3, 4, 6, 7, 8, 9, 10, 12 };
+
+        for (int i = 0; i < 13; i++) {
+            System.out.printf("%d : %d%n", i, find(i, data));
+        }
+    }
+}
