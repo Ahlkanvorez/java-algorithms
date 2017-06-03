5fb64cebe24efc3db58aca1211da984d8870d444
Robert Mitchell <robert.mitchell36@gmail.com>
2017-06-03 13:28:09 -0500

    Added unit tests for QuickSort

---
+++ Algorithms/test/algs/sorts/QuickSortTest.java
@@ -0,0 +1,13 @@
+package algs.sorts;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Created by robertmitchell on 6/3/17.
+ */
+class QuickSortTest extends SortTest {
+    QuickSortTest() {
+        super();
+        super.sort = new QuickSort<>();
+    }
+}
